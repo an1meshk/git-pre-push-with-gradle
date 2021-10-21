5e5cf0ff771eb0e15362d5c205819db145649665
Animesh <an1meshk@users.noreply.github.com>
2021-10-21 15:07:12 -0500

    Initial Commit

---
+++ src/main/java/com/akdevcraft/gitprepush/GitPrePushWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.akdevcraft.gitprepush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPrePushWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPrePushWithGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akdevcraft/gitprepush/GitPrePushWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akdevcraft.gitprepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPrePushWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
